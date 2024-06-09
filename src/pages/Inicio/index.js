7e3bb30d6543b082be2a750f3eaba624d60ef151
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-09 16:58:17 -0300

    titulo component

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,12 +1,16 @@
 import Banner from "components/Banner/Banner";
 import Cabecalho from "components/Cabecalho/Cabecalho";
 import Rodape from "components/Rodape/Rodape";
+import Titulo from "components/Titulo/Titulo";
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="home"/>
+            <Titulo>
+                <h1>Um ligar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
             <Rodape />
         </>
     )
