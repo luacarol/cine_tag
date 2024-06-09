d3cb4b7fee406e7cb36bfe3111ca109b06902827
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-09 17:41:55 -0300

    container component

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,18 @@ import Favoritos from "pages/Favoritos";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cabecalho from "components/Cabecalho/Cabecalho";
 import Rodape from "components/Rodape/Rodape";
+import Container from "components/Container/Container";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Cabecalho />
+            <Container>
             <Routes>
                 <Route path="/" element={<Inicio />}></Route>
                 <Route path="/favoritos" element={<Favoritos />}></Route>
             </Routes>
+            </Container>
             <Rodape />
         </BrowserRouter>
     )
