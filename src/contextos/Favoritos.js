4329ae96f80326a7a5035b2aa635c49e35fd3d89
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-10 18:39:50 -0300

    use context

--- src/contextos/Favoritos.js
+++ src/contextos/Favoritos.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const FavoritosContext = createContext();
 FavoritosContext.displayName = "Favoritos";
@@ -9,4 +9,27 @@ export default function FavoritosProvider({ children }) {
     return (
         <FavoritosContext.Provider value={{favorito, setFavorito}}>{children}</FavoritosContext.Provider>
     )
+}
+
+export function useFavoritoContext() {
+    const { favorito, setFavorito } = useContext(FavoritosContext);
+
+    function adicionarFavorito(novoFavorito) {
+        const favoritoRepetido = favorito.some(item => item.id === novoFavorito.id);
+
+        let novaLista = [...favorito];
+
+        if (!favoritoRepetido) {
+            novaLista.push(novoFavorito)
+            return setFavorito(novaLista);
+        }
+
+        novaLista.splice(novaLista.indexOf(novoFavorito), 1);
+        return setFavorito(novaLista);
+    }    
+
+    return {
+        favorito,
+        adicionarFavorito
+    }
 }
