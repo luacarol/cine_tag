796251dc28c6de24c3850ddec1e4c66434748707
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-10 18:10:34 -0300

    favoritos component

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,18 @@
+import Banner from 'components/Banner/Banner';
 import styles from './Favoritos.module.css';
+import Titulo from 'components/Titulo/Titulo';
+import Card from 'components/Card/Card';
 
 function Favoritos() {
     return (
         <>
-        
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id="1" titulo="Gato bonifácio" capa="https://thecatapi.com/api/images/get?format=src&type=png" ></Card>
+            </section>
         </>
     )
 }
